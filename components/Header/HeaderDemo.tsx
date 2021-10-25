b33d4a872a33571787434883284c79c8449c2d9d
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-10-25 12:45:40 -0700

    fixed build issues;

--- components/Header/HeaderDemo.tsx
+++
@@ -1,9 +0,0 @@
-import Button from "../Button";
-import Content from "../Content";
-import Header from "../Header";
-import Title from "../Title";
-import Wrapper from "../Wrapper";
-
-const HeaderDemo = () => <Header></Header>;
-
-export default HeaderDemo;
